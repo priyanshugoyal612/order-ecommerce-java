326ac2531a4b16146149eb398629103b97a07972
priyanshu.goyal <priyanshu.goyal@ukg.com>
2022-12-10 11:34:13 +0530

    1 comment

---
+++ src/main/java/com/egen/orderecommercejava/OrderEcommerceJavaApplication.java
@@ -0,0 +1,13 @@
+package com.egen.orderecommercejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderEcommerceJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderEcommerceJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/egen/orderecommercejava/OrderEcommerceJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egen.orderecommercejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderEcommerceJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
